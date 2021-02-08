9d09dc6668d9b13757c0ccb188b29d6fabbb3c6b
Bryant Shao <57240791+IntelBear@users.noreply.github.com>
2021-02-08 10:35:45 -0800

    Create hi.js

---
+++ src/hi.js
@@ -0,0 +1,18 @@
+/** 
+ * Does nothing
+ * @author IntelBear
+ * @param {null} nothing - Does nothing
+ * @returns void
+ */
+function doNothing (nothing){
+    console.log("Nothing");
+}
+
+/**
+ * Returns
+ * @author Me
+ * @returns void
+ */
+function hi (){
+    return;
+}
