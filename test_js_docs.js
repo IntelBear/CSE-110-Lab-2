cf6992c5f83e0b249e86b7410e9d9bfa674cb2b7
IntelBear <bryantshao@gmail.com>
2021-02-07 23:12:35 -0800

    Added documentation

--- test_js_docs.js
+++ test_js_docs.js
@@ -1,3 +1,9 @@
-function doNothing (){
+/** 
+ * Does nothing
+ * @author IntelBear
+ * @param {null} nothing - Does nothing
+ * @return void
+ */
+function doNothing (nothing){
     console.log("Nothing");
 }
