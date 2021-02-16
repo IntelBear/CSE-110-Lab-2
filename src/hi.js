aa92d48999d573bf970ce98fb6e5fcdab17ff981
IntelBear <bryantshao@gmail.com>
2021-02-15 21:46:15 -0800

    Testing deploy

--- src/hi.js
+++ src/hi.js
@@ -1,13 +1,3 @@
-/** 
- * Does nothing
- * @author IntelBear
- * @param {null} nothing - Does nothing
- * @returns void
- */
-function doNothing (nothing){
-    console.log("Nothing");
-}
-
 /**
  * Returns
  * @author Me
